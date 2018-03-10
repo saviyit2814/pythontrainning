8f8ab7edee5aa3544b71995dc46c9756d585bb0a
saviyit2814 <37235622+saviyit2814@users.noreply.github.com>
2018-03-10 15:30:52 +0530

    Add files via upload

---
+++ assignment/add.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module4
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#------
+-----------------------------------------
+
+counter=100
+miles  =1000.0
+name   ="John"
+print counter
+print miles
+print name)
---
+++ assignment/module1.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     09/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ c=[20,10,1,2,3,4,5,6,7,8]
+ a=10
+ b=8
+ print(a+b)
+ e=20
+ f=1
+ g=e-f
+ print(g)
+ p=8
+ q=4
+ r=p/q
+ print(r)
+ z=1
+ print(z in c)
+ a=6
+ b=8
+ c=a*b
+ print(c)
+ a=20
+ b=3
+ c=a%b
+ print(c)
+ a=3
+ b=4
+ print(a<>b)
+ a=3
+ b=3
+ print(a==b)
+
+if __name__ == '__main__':
+    main()
